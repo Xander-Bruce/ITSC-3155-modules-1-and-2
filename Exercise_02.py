d473716001f3f4632efc9f8f4cc22b4aeb8a1e4e
Xander Bruce <zbruce3@uncc.edu>
2024-01-21 11:43:22 -0500

    Added in comments for the second problem.

--- Exercise_02.py
+++ Exercise_02.py
@@ -1,19 +1,25 @@
 
+#Function for taking a string and sorting it in the order of the lower case, upper case, and other characters
 def string_to_lowerUpper(a : str) -> str:
     lower = []
     upper = []
     other = []
     final : str = ""
 
+    # For every character in the string
     for x in a:
+
         if x.islower():
             lower.append(x)
+
         elif x.isupper():
             upper.append(x)
+
         elif not x.isspace():
             other.append(x)
         #Anything else is a space
 
+    #Adding in the characters in the order, lower case, upper case, and any other character
     for y in lower:
         final += y
 
@@ -28,10 +34,14 @@ def string_to_lowerUpper(a : str) -> str:
 #
 #Program Start
 #
+
+#Getting user input
 while True:
     try:
 
         s = input("\nPlease input a string: ")
+
+        #Throwing an error if the input is just a space
         if s.isspace():
             raise ValueError
         
