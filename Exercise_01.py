5c7db0b7a0e7c79aac7861d99dda1a3fbc0c3d08
Xander Bruce <zbruce3@uncc.edu>
2024-01-21 11:36:37 -0500

    Added in comments.

--- Exercise_01.py
+++ Exercise_01.py
@@ -1,16 +1,23 @@
 
+#takes in a string and then outputs a copy of the string but backwards
 def reverse_String(a : str) -> str:
+
     i = len(a)
     out = ''
+
     while i > 0:
         out += a[i-1]
         i-=1
+
     return out
 
+#gets a user inputted string
 while True:
     try:
 
         string : str = input("\nEnter a String: ")
+
+        #Throws an error if the user input is just a space
         if string.isspace():
             raise ValueError
 
